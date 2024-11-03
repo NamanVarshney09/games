b527308574213d2dbaf43aadb7df4627f9d06163
Naman Varshney <namanvarshney09@gmail.com>
2024-11-03 16:53:02 +0530

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,218 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import { FiArrowRight, FiMail, FiBriefcase } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
+            <Logo />
+            <motion.div
+                initial="initial"
+                animate="animate"
+                transition={{
+                    staggerChildren: 0.05,
+                }}
+                className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4"
+            >
+                <HeaderBlock />
+                <GamesBlock />
+                <AboutBlock />
+                <PortfolioBlock />
+                <EmailListBlock />
+            </motion.div>
+            <Footer />
+        </div>
+    );
+};
+
+const Block = ({ className, ...rest }) => {
+    return (
+        <motion.div
+            variants={{
+                initial: {
+                    scale: 0.5,
+                    y: 50,
+                    opacity: 0,
+                },
+                animate: {
+                    scale: 1,
+                    y: 0,
+                    opacity: 1,
+                },
+            }}
+            transition={{
+                type: "spring",
+                mass: 3,
+                stiffness: 400,
+                damping: 50,
+            }}
+            className={twMerge(
+                "col-span-4 rounded-lg border border-zinc-700 bg-zinc-800 p-6",
+                className
+            )}
+            {...rest}
+        />
+    );
+};
+
+const HeaderBlock = () => (
+    <Block className="col-span-12 row-span-2 md:col-span-6">
+        <img
+            src="https://api.dicebear.com/8.x/lorelei-neutral/svg?seed=John"
+            alt="avatar"
+            className="mb-4 size-14 rounded-full"
+        />
+        <h1 className="mb-12 text-4xl font-medium leading-tight">
+            Hi, I'm Naman.{" "}
+            <span className="text-zinc-400">
+                I build cool websites like this one.
+            </span>
+        </h1>
+        <a
+            href="https://namanvarshney.vercel.app/#contact" target="_blank"
+            className="flex items-center gap-1 text-red-300 hover:underline"
+        >
+            Contact me <FiArrowRight />
+        </a>
+    </Block>
+);
+
+const GamesBlock = () => (
+    <>
+        <Block
+            whileHover={{
+                rotate: "2.5deg",
+                scale: 1.1,
+            }}
+            className="col-span-6 bg-red-500 md:col-span-3"
+        >
+            <Link
+                to="/memory-game"
+                className="grid h-full place-content-center text-3xl text-white"
+            >
+                {/* <SiYoutube /> */}
+                <span className="font-bold text-center">
+                    Memory Game
+                    </span>
+            </Link>
+        </Block>
+        <Block
+            whileHover={{
+                rotate: "-2.5deg",
+                scale: 1.1,
+            }}
+            className="col-span-6 bg-green-600 md:col-span-3"
+        >
+            <a
+                href="#"
+                className="grid h-full place-content-center text-3xl text-white"
+            >
+                {/* <SiGithub /> */}
+            </a>
+        </Block>
+        <Block
+            whileHover={{
+                rotate: "-2.5deg",
+                scale: 1.1,
+            }}
+            className="col-span-6 bg-zinc-50 md:col-span-3"
+        >
+            <a
+                href="#"
+                className="grid h-full place-content-center text-3xl text-black"
+            >
+                {/* <SiTiktok /> */}
+            </a>
+        </Block>
+        <Block
+            whileHover={{
+                rotate: "2.5deg",
+                scale: 1.1,
+            }}
+            className="col-span-6 bg-blue-500 md:col-span-3"
+        >
+            <a
+                href="#"
+                className="grid h-full place-content-center text-3xl text-white"
+            >
+                {/* <SiTwitter /> */}
+            </a>
+        </Block>
+    </>
+);
+
+const AboutBlock = () => (
+    <Block className="col-span-12 text-3xl leading-snug">
+        <p>
+            My passion is building cool stuff.{" "}
+            <span className="text-zinc-400">
+                I build primarily with React, Express, MongoDB, Tailwind CSS, and Framer Motion.
+            </span>
+        </p>
+    </Block>
+);
+
+const PortfolioBlock = () => (
+    <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
+        <a href="https://namanvarshney.vercel.app" target="_blank"><FiBriefcase className="text-3xl" /></a>
+        <a href="https://namanvarshney.vercel.app" target="_blank" className="text-center text-lg text-zinc-400">
+            Portfolio
+        </a>
+    </Block>
+);
+
+const EmailListBlock = () => (
+    <Block className="col-span-12 md:col-span-9">
+        <p className="mb-3 text-lg">Join my mailing list</p>
+        <form
+            onSubmit={(e) => e.preventDefault()}
+            className="flex items-center gap-2"
+        >
+            <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-full rounded border border-zinc-700 bg-zinc-800 px-3 py-1.5 transition-colors focus:border-red-300 focus:outline-0"
+            />
+            <button
+                type="submit"
+                className="flex items-center gap-2 whitespace-nowrap rounded bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-300"
+            >
+                <FiMail />
+                Join the list
+            </button>
+        </form>
+    </Block>
+);
+
+const Logo = () => {
+    return (
+        <div className="flex items-center justify-center mb-10">
+            <svg id="logo-72"
+                width="52"
+                height="44"
+                viewBox="0 0 53 44"
+                fill="#000000"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path d="M23.2997 0L52.0461 28.6301V44H38.6311V34.1553L17.7522 13.3607L13.415 13.3607L13.415 44H0L0 0L23.2997 0ZM38.6311 15.2694V0L52.0461 0V15.2694L38.6311 15.2694Z" className="ccustom" fill="#ffffff"></path>
+            </svg>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer className="mt-12">
+            <p className="text-center text-zinc-400">
+                Made with ❤️ by{" "}
+                <a href="https://www.linkedin.com/in/namanvarshney09/" target="_blank" className="text-red-300 hover:underline">
+                    @namanvarshney09
+                </a>
+            </p>
+        </footer>
+    );
+};
+
+export default Home;
---
+++ src/components/Memory.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+const Memory = () => {
+  const [gridSize, setGridSize] = useState(2);
+  const [cards, setCards] = useState([])
+  const [flipped, setFlipped] = useState([])
+  const [solved, setSolved] = useState([])
+  const [disabled, setDisabled] = useState(false)
+  const [inProgress, setInProgress] = useState(false);
+
+  useEffect(() => {
+    let totalCards = gridSize * gridSize;
+    const numbers = [...Array(Math.floor(totalCards/2))].map((_, i) => i + 1);
+    const shuffledCards = [...numbers, ...numbers]
+      .sort(() => Math.random() - 0.5)
+      .map((number, index) => ({ id: index, number })); 
+    setCards(shuffledCards);
+  }, [gridSize])
+
+
+  return (
+    <>
+      <div className="flex flex-col gap-1 h-screen w-screen items-center justify-start bg-red-100">
+        <h2 className='text-4xl font-bold mb-8'>Memory Game</h2>
+        <label htmlFor="gridSize" className='font-semibold'>Grid Size</label>
+        <input type="number" name="gridSize" className='bg-green-100 px-4 py-2 font-bold w-20 rounded-lg' value={gridSize} onChange={(e) => setGridSize(Number(e.target.value))} min={2} max={10} disabled={inProgress}/>
+        {inProgress && <div>
+          {cards.map((card) => {
+            return <div key={card.id}>  
+              {card.number}
+            </div>
+          })}
+        </div>}
+        {/* Message */}
+
+        <button onClick={()=>(setInProgress(prev => !prev))} type="button" className="mt-5 px-4 py-2 bg-blue-500 text-white font-bold rounded-lg cursor-pointer hover:scale-105">{inProgress ? "Reset" : "Start"}</button>
+      </div>
+    </>
+  )
+}
+
+export default Memory
